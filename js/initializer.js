d56d1a7db7570bce6f9da41452e8aa875210392b
Guillem Bagur <guillembagurmoll@gmail.com>
2023-02-22 21:02:20 +0100

    Modify server url

--- js/initializer.js
+++ js/initializer.js
@@ -4,4 +4,4 @@ const keys = {
     ocr: "bWNAku3qXipL0kM4bhpGoDrcfS0K0P9t"
 };
 
-const serverURL = "http://localhost:3002" || "https://angry-tan-vulture.cyclic.app";
+const serverURL = "https://talvi.cyclic.app" ||  "http://localhost:3002";
