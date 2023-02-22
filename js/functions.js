02f3dc0563ef4b8aa6e1f58c041438ead9e544c2
Guillem Bagur <guillembagurmoll@gmail.com>
2023-02-22 21:01:09 +0100

    Upload images via form

--- js/functions.js
+++ js/functions.js
@@ -1,25 +1,7 @@
-const levenshteinDistance = (s, t) => {
-    if (!s.length) return t.length;
-    if (!t.length) return s.length;
-    const arr = [];
-    for (let i = 0; i <= t.length; i++) {
-      arr[i] = [i];
-      for (let j = 1; j <= s.length; j++) {
-        arr[i][j] =
-          i === 0
-            ? j
-            : Math.min(
-                arr[i - 1][j] + 1,
-                arr[i][j - 1] + 1,
-                arr[i - 1][j - 1] + (s[j - 1] === t[i - 1] ? 0 : 1)
-              );
-      }
-    }
-    return arr[t.length][s.length];
-  };
-
-
-
+const sendForm = id => {
+  const form = document.getElementById(id);
+  form.submit();
+}
 
 const loadFile = async path => {
     const file = await fetch(path, 
@@ -45,7 +27,7 @@ const removeAccents = (str) => {
 const fetchStats = async (userID) => {
   try {
     if(!userID) return undefined;
-    const res = await fetch(`${serverURL}/purchases/${userID}`);
+    const res = await loadFile(`${serverURL}/purchases/${userID}`);
     const userPurchases = await res.json();
     if(!userPurchases) return undefined;
     const currentDate = new Date();
--- js/initializer.js
+++ js/initializer.js
@@ -4,4 +4,4 @@ const keys = {
     ocr: "bWNAku3qXipL0kM4bhpGoDrcfS0K0P9t"
 };
 
-const serverURL = "https://angry-tan-vulture.cyclic.app";
+const serverURL = "http://localhost:3002" || "https://angry-tan-vulture.cyclic.app";
